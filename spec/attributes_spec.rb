c206785911b626c0de3c357f627ace21ff06d9fd
Grant Heffernan <heffergm@gmail.com>
2014-03-06 12:18:28 -0500

    spec

--- spec/attributes_spec.rb
+++ spec/attributes_spec.rb
@@ -10,12 +10,14 @@ describe 'mapzen_tilestache::default' do
     end
   end
 
-  it "should correctly set the config source file to tilestache-test.conf.erb when env is test" do
-    stub_command("test -f /opt/logstash/bin/logstash-1.3.3-flatjar.jar").and_return(true)
-    chef_run.node.set[:mapzen][:environment] = 'test'
-    chef_run.converge(described_recipe)
+  %w(dev test stage prod).each do |env|
+    it "should correctly set the config source file to tilestache-#{env}.conf.erb when env is #{env}" do
+      stub_command("test -f /opt/logstash/bin/logstash-1.3.3-flatjar.jar").and_return(true)
+      chef_run.node.set[:mapzen][:environment] = env
+      chef_run.converge(described_recipe)
 
-    expect(chef_run.node[:tilestache][:config][:source_file]).to eq("tilestache-#{env}.conf.erb")
+      expect(chef_run.node[:tilestache][:config][:source_file]).to eq("tilestache-#{env}.conf.erb")
+    end
   end
 
 end
