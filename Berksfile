362eb3f5f3275a893bbe598035c6121b432e689e
Grant Heffernan <heffergm@gmail.com>
2014-03-06 09:15:44 -0500

    specs

--- Berksfile
+++ Berksfile
@@ -1,5 +1,8 @@
-site :opscode
+metadata
 
-cookbook 'tilestache', git: 'git@github.com:mapzen/chef-tilestache.git'
-cookbook 'mapzen_tilestache', :path => '.'
-
+# mapzen wrappers
+#
+cookbook 'mapzen_sensu_clients' , github: 'mapzen/chef-mapzen_sensu_clients'  , tag: '0.2.3'
+cookbook 'mapzen_logstash'      , github: 'mapzen/chef-mapzen_logstash'       , tag: '0.2.2'
+cookbook 'mapzen_graphite'      , github: 'mapzen/chef-mapzen_graphite'       , tag: '0.1.2'
+cookbook 'tilestache'           , github: 'mapzen/chef-tilestache'            , tag: '0.10.0'
---
+++ spec/default_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+
+describe 'tilestache::default' do
+  context 'we are assigned layer tilestache' do
+    let (:chef_run) { ChefSpec::Runner.new.converge(described_recipe) }
+
+    %w(
+      tilestache::default
+      mapzen_tilestache::queries
+      mapzen_tilestache::sensu
+      mapzen_tilestache::logstash
+    ).each do |recipe|
+      it "should include recipe #{recipe}" do
+        chef_run.should include_recipe recipe
+      end
+    end
+
+  end
+
+  context 'we are not assigned layer tilestache do' do
+    let (:chef_run) { ChefSpec::Runner.new.converge(described_recipe) }
+
+    %w(
+      tilestache::default
+      mapzen_tilestache::queries
+      mapzen_tilestache::sensu
+      mapzen_tilestache::logstash
+    ).each do |recipe|
+      it "should not include recipe #{recipe}" do
+        chef_run.should_not include_recipe recipe
+      end
+    end
+
+  end
+
+end
