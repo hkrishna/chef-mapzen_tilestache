4a0efb61c1e18d1e59b1c282e353febcbd315294
Grant Heffernan <heffergm@gmail.com>
2014-03-17 16:09:17 -0400

    rename buildings-lite to buildingslite so logstash will pick up the addition without any crazy pattern matching

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'grant@mapzen.com'
 license          'All rights reserved'
 description      'Installs/Configures tilestache'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.11.11'
+version          '0.11.12'
 
 recipe 'mapzen_tilestache', 'Wraps tilestache'
 
